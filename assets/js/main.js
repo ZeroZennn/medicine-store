d9855cfa5696c1857e5c02cb6ce317f87f980c28
Luft21 <jfebryan15@gmail.com>
2024-07-02 19:33:26 +0700

    header template, modal, home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,35 @@
+function fetchDB(table) {
+  return fetch(`data/${table}.json`);
+}
+
+function userLogin(username, password) {
+    console.log(fetchDB("user"));
+
+}
+
+function userRegister(username, password) {
+    
+}
+
+function handleLogin() {
+  
+}
+
+function handleRegister() {
+  alert("test");
+}
+
 function getProductsData() {
-    return fetch('data.json')
-      .then(response => response.json())
-      .then(data => data);
-  }
-  
-  const fetchProductsButton = document.getElementById('fetch-products-btn');
-  const productsContainer = document.getElementById('products-container');
-  
-  fetchProductsButton.addEventListener('click', () => {
-    getProductsData().then(data => {
-      console.log(data);
-    });
+  return fetch('data/products.json')
+    .then(response => response.json())
+    .then(data => data);
+}
+
+const fetchProductsButton = document.getElementById('fetch-products-btn');
+const productsContainer = document.getElementById('products-container');
+
+fetchProductsButton.addEventListener('click', () => {
+  getProductsData().then(data => {
+    console.log(data);
   });
+});
---
+++ templates/header.js
@@ -0,0 +1,68 @@
+const header = document.querySelector("header");
+
+header.innerHTML = `
+        <link rel="stylesheet" href="assets/css/header.css">
+        <!-- overlay -->
+        <div id="overlay" class="fixed hidden z-40 w-screen h-screen inset-0 bg-gray-900 bg-opacity-60"></div>
+    
+        <!-- login register dialog -->
+        <div id="dialog" class="hidden fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 bg-white rounded-md px-8 py-6 space-y-5 drop-shadow-lg">
+            <h1 class="text-2xl font-semibold">Login Test</h1>
+
+            <div class="flex justify-end">
+                <button type="button" onclick="loginhandle()" class="mx-2 px-5 py-2 bg-blue-500 hover:bg-blue-700 text-white cursor-pointer rounded-md">
+                    Login
+                </button>
+                <button id="close" class="mx-2 px-5 py-2 bg-red-500 hover:bg-red-700 text-white cursor-pointer rounded-md">
+                    Close
+                </button>
+            </div>
+        </div>
+
+        <!-- navbar -->
+        <nav class="bg w-full">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1">
+                <a href="./" class="flex items-center">
+                    <img src="assets/svg/logo.svg" class="h-8" alt="MediCran Logo">
+                </a>
+                <div class="items-center justify-between">
+                    <ul class="flex flex-row font-medium">
+                        <li>
+                            <a href="./" class="block py-2 mx-2 text-white">Home</a>
+                        </li>
+                        <li>
+                            <a href="products.html" class="block py-2 mx-2 text-white">Products</a>
+                        </li>
+                        <li>
+                            <a href="about.html" class="block py-2 mx-2 text-white">About Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="flex">
+                    <button id="login_open" type="button" class="font-medium text-white">Login</button>
+                </div>
+            </div>
+        </nav>
+`
+
+var login = document.getElementById('login_open');
+var dialog = document.getElementById('dialog');
+var closeButton = document.getElementById('close');
+var overlay = document.getElementById('overlay');
+
+login.addEventListener("click", function() {
+    dialog.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+});
+
+closeButton.addEventListener('click', function () {
+    dialog.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
+
+window.onclick = function(event) {
+  if (event.target == overlay) {
+    dialog.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+}
